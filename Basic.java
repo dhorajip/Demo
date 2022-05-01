41f08c2c8d9fabb841d8e53c17f45b25b160065c
dhorajip <104282993+dhorajip@users.noreply.github.com>
2022-05-01 14:39:40 -0400

    Add files via upload
    First Upload

---
+++ Basic.java
@@ -0,0 +1,13 @@
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Selenium");
+		System.out.println("Test");
+		System.out.println("1");
+		System.out.print("2");
+		
+	}
+
+}
---
+++ one.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class one {
+
+	public static void main(String[] args) {
+		System.out.println("Test Data:");
+		Scanner s = new Scanner(System.in);
+		
+		
+		System.out.println("Input first number: ");
+		int a = s.nextInt();
+		System.out.println("Input another number: ");
+		int b = s.nextInt();
+		System.out.println("The Multiplication is: " + a*b);
+		
+
+	
+	}
+
+}
